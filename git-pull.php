3adbc87edc98016f709b127ef27682a018da83f5
Mayuko Moriyama <mayuko.jpn@gmail.com>
2021-08-13 00:33:26 +0900

    Initial commit

---
+++ git-pull.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Plugin Name:  git pull
+ * Description:  An instant git controller for small theme development environments.
+ * Version:      0.1
+ * Author:       Mayo Moriyama
+ * Author URI:   https://github.com/mayukojpn
+ * License:      GPLv2 or later
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package git-pull
+ */
+
+class Git_Pull {
+
+	public $admin_notices = [];
+  public function __construct() {
+    add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+  }
+
+  public function admin_menu() {
+  	$theme_page = add_theme_page(
+  		'git pull',
+			'git pull',
+  		'edit_theme_options',
+  		'git_pull',
+  		array( $this, 'exec_command' )
+  	);
+  }
+
+  public function exec_command() {
+		$theme   = wp_get_theme();
+		$slug    = escapeshellcmd($theme->stylesheet);
+		$command = "git -C ../wp-content/themes/{$slug}/ pull origin main";
+		exec($command, $output);
+		if ( empty( $output ) ) {
+			$this->admin_notices[] = array(
+				'status'  => 'error',
+				'content' => "Error: <b>No renponse from server</b>.",
+			);
+		}
+		else {
+			$this->admin_notices[] = array(
+				'status'  => 'success',
+				'content' => 'Successfully git pull command worked: <b>' . $output[0] . '</b>',
+			);
+		}
+    self::admin_notices();
+  }
+
+	public function admin_notices() {
+		echo '<h1 class="wp-heading-inline">git pull</h1>';
+		if ( empty( $this->admin_notices ) ) {
+			return;
+		}
+		foreach ( $this->admin_notices as $notice ) {
+			?>
+			<div class="notice notice-<?php echo esc_attr( $notice['status'] ); ?>">
+				<p><?php echo $notice['content']; // WPCS: XSS OK. ?></p>
+			</div>
+			<?php
+		}
+	}
+}
+new Git_Pull();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# git pull
+
+* Contributors: mayukojpn
+* Tags:
+* Requires at least: 5.5
+* Tested up to: 5.7
+* Requires PHP: 5.5.9
+* Stable tag: 0.1
+* License: GPLv2 or later
+* License URI: <http://www.gnu.org/licenses/gpl-2.0.html>
+
+An instant git controller for small theme development environments.
+
+## Description
+An instant git controller for small theme development environments where you haven't set up automated tools but still need to access Git using UI.
